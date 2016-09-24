611813e3255ae0696f163dbb3d7cbb44d4bb8615
VladVladimirov <vlad23vladimirov@gmail.com>
2016-09-24 16:40:17 +0700

    init commit

---
+++ app/src/main/java/com/course/vlad/jadcourse/MainActivity.java
@@ -0,0 +1,20 @@
+package com.course.vlad.jadcourse;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Vlad on 9/24/2016.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String appName = getString(R.string.app_name);
+
+    }
+}
